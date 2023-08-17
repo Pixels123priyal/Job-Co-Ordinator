8a48513732d4bb4bc02ff35588cd0ee4950c5d3c
Pixels123priyal <85298332+Pixels123priyal@users.noreply.github.com>
2023-08-17 15:24:24 +0530

    initial commit

---
+++ Online-job-portal-for-unorganised-labour/README.md
@@ -0,0 +1,1 @@
+# Online-job-portal-for-unorganised-labour
---
+++ Online-job-portal-for-unorganised-labour/frontend/src/lib/apiList.js
@@ -0,0 +1,15 @@
+export const server = "https://online-job-portal-for-unorganised-labour-ouu2.onrender.com";
+
+const apiList = {
+  login: `${server}/auth/login`,
+  signup: `${server}/auth/signup`,
+  uploadResume: `${server}/upload/resume`,
+  uploadProfileImage: `${server}/upload/profile`,
+  jobs: `${server}/api/jobs`,
+  applications: `${server}/api/applications`,
+  rating: `${server}/api/rating`,
+  user: `${server}/api/user`,
+  applicants: `${server}/api/applicants`,
+};
+
+export default apiList;
